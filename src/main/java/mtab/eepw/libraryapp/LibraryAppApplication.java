5ab3140e5a27245b071fb630c14c27daf06ca263
AdamKw <adam.kwapisz123@gmail.com>
2023-11-09 09:57:44 +0100

    initial commit

---
+++ src/main/java/mtab/eepw/libraryapp/LibraryAppApplication.java
@@ -0,0 +1,13 @@
+package mtab.eepw.libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+
+}
